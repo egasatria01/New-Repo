8999bad9fcc806d40c38b959b9df6daa3c4e4091
anggarivian <anggarivian.11@gmail.com>
2023-01-14 11:21:25 +0700

    add pdf pasien

--- resources/views/laporan.blade.php
+++ resources/views/laporan.blade.php
@@ -10,76 +10,14 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        <div class="col-4 border">
+        <div class="col-12 border">
             <div class="form-group">
                 <div class="bg-primary p-2 mb-3 text-center">
                     <label for="penulis">Laporan Pasien</label>
                 </div>
-                <div class="row">
-                    <div class="col-6">
-                    <div class="form-group">
-                    <label for="tahun"></label>
-                            <select name="province" class="form-control">
-                            <option value="">Sampai Saat Ini</option>
-                            <option value="">Per Tanggal</option>
-                            <option value="">Bulan</option>
-                            <option value="">Tahun</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-6">
+                    <div class="col-4">
                         <div class="modal-footer">
-                            <button type="submit" class="btn btn-outline-primary w-100">Pilih</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-4 border">
-            <div class="form-group">
-                <div class="bg-primary p-2 mb-3 text-center">
-                    <label for="penulis">Laporan Dokter</label>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                    <div class="form-group">
-                    <label for="tahun"></label>
-                            <select name="province" class="form-control">
-                            <option value="">Sampai Saat Ini</option>
-                            <option value="">Per Tanggal</option>
-                            <option value="">Bulan</option>
-                            <option value="">Tahun</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-outline-primary w-100">Pilih</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        {{-- <div class="col-4 border">
-            <div class="form-group">
-                <div class="bg-primary p-2 mb-3 text-center">
-                    <label for="penulis">Laporan User</label>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                    <div class="form-group">
-                    <label for="tahun"></label>
-                            <select name="province" class="form-control">
-                            <option value="">Sampai Saat Ini</option>
-                            <option value="">Per Tinggal</option>
-                            <option value="">Bulan</option>
-                            <option value="">Tahun</option>
-                            </select>
-                        </div>
-                    </div> --}}
-                    <div class="col-6">
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-outline-primary w-100">Pilih</button>
+                            <a href="{{ route('admin.print.pasien')}}" target="_blank" class="btn btn-outline-primary w-100"><i class="fa fa-print"></i>Cetak PDF</a>
                         </div>
                     </div>
                 </div>
@@ -94,24 +32,13 @@
     <div class="row justify-content-center">
         <div class="col-4 border">
             <div class="form-group">
-                <div class="bg-success p-2 mb-3 text-center">
+                <div class="bg-danger p-2 mb-3 text-center">
                     <label for="penulis">Laporan kamar</label>
                 </div>
                 <div class="row">
                     <div class="col-6">
-                    <div class="form-group">
-                    <label for="tahun"></label>
-                            <select name="province" class="form-control">
-                            <option value="">Sampai Saat Ini</option>
-                            <option value="">Per Tinggal</option>
-                            <option value="">Bulan</option>
-                            <option value="">Tahun</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-6">
                         <div class="modal-footer">
-                            <button type="submit" class="btn btn-outline-success w-100">Pilih</button>
+                            <button type="submit" class="btn btn-outline-danger w-100">Pilih</button>
                         </div>
                     </div>
                 </div>
@@ -119,103 +46,10 @@
         </div>
         <div class="col-4 border">
             <div class="form-group">
-                <div class="bg-success p-2 mb-3 text-center">
+                <div class="bg-danger p-2 mb-3 text-center">
                     <label for="penulis">Laporan Spesialis</label>
                 </div>
                 <div class="row">
-                    <div class="col-6">
-                    <div class="form-group">
-                    <label for="tahun"></label>
-                            <select name="province" class="form-control">
-                            <option value="">Sampai Saat Ini</option>
-                            <option value="">Per Tinggal</option>
-                            <option value="">Bulan</option>
-                            <option value="">Tahun</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-outline-success w-100">Pilih</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<br>
-<br>
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-4 border">
-            <div class="form-group">
-                <div class="bg-danger p-2 mb-3 text-center">
-                    <label for="penulis">Laporan Hasil Periksa</label>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                    <div class="form-group">
-                    <label for="tahun"></label>
-                            <select name="province" class="form-control">
-                            <option value="">Sampai Saat Ini</option>
-                            <option value="">Per Tinggal</option>
-                            <option value="">Bulan</option>
-                            <option value="">Tahun</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-outline-danger w-100">Pilih</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-4 border">
-            <div class="form-group">
-                <div class="bg-danger p-2 mb-3 text-center">
-                    <label for="penulis">Laporan Perawatan</label>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                    <div class="form-group">
-                    <label for="tahun"></label>
-                            <select name="province" class="form-control">
-                            <option value="">Sampai Saat Ini</option>
-                            <option value="">Per Tinggal</option>
-                            <option value="">Bulan</option>
-                            <option value="">Tahun</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-outline-danger w-100">Pilih</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-4 border">
-            <div class="form-group">
-                <div class="bg-danger p-2 mb-3 text-center">
-                    <label for="penulis">Laporan Pembayaran</label>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                    <div class="form-group">
-                    <label for="tahun"></label>
-                            <select name="province" class="form-control">
-                            <option value="">Sampai Saat Ini</option>
-                            <option value="">Per Tinggal</option>
-                            <option value="">Bulan</option>
-                            <option value="">Tahun</option>
-                            </select>
-                        </div>
                     </div>
                     <div class="col-6">
                         <div class="modal-footer">
@@ -227,4 +61,6 @@
         </div>
     </div>
 </div>
+<br>
+<br>
 @endsection
